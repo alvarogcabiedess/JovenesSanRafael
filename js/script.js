a15aea440c503bfa14ea869654ae45e5757f63bc
Álvaro Gutiérrez de Cabiedes Gonzalvo <128607961+alvarogcabiedess@users.noreply.github.com>
2025-09-15 13:12:51 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Script para mostrar/ocultar un menú lateral si deseas
+document.getElementById('menuToggle').addEventListener('click', () => {
+  alert('Aquí podrías abrir un menú lateral con más opciones');
+});
